3f42f6f3e9602d54c18740494210cdfb83c314cf
pUmA73 <stefanpp77@yahoo.com>
2024-04-20 22:17:29 +0300

    Added LeaderboardDAO and RunDAO classes

--- Cod/app/src/main/java/com/example/runpath/database/LeaderboardDAO.kt
+++ Cod/app/src/main/java/com/example/runpath/database/LeaderboardDAO.kt
@@ -1,4 +1,67 @@
 package com.example.runpath.database
 
-class LeaderboardDAO {
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import kotlin.time.Duration
+
+class LeaderboardDAO(context: Context, dbHelper: SQLiteOpenHelper) {
+
+    private val db: SQLiteDatabase = dbHelper.writableDatabase
+
+    fun insertLeaderboard(
+        leaderboardId: Int,
+        circuitId: Int,
+        userId: Int,
+        rank: Int,
+        time: Duration
+    ): Long {
+        val values = ContentValues().apply {
+            put(DataBase.LeaderboardEntry.COLUMN_LEADERBOARD_ID, leaderboardId)
+            put(DataBase.LeaderboardEntry.COLUMN_CIRCUIT_ID, circuitId)
+            put(DataBase.LeaderboardEntry.COLUMN_USER_ID, userId)
+            put(DataBase.LeaderboardEntry.COLUMN_RANK, rank)
+            put(DataBase.LeaderboardEntry.COLUMN_TIME, time.toString())
+        }
+        return db.insert(DataBase.LeaderboardEntry.TABLE_NAME, null, values)
+    }
+
+    fun getLeaderboardById(leaderboardId: Int): Cursor {
+        return db.rawQuery(
+            "SELECT * FROM ${DataBase.LeaderboardEntry.TABLE_NAME} WHERE ${DataBase.LeaderboardEntry.COLUMN_LEADERBOARD_ID} = ?",
+            arrayOf(leaderboardId.toString())
+        )
+    }
+
+    fun updateLeaderboard(
+        leaderboardId: Int,
+        circuitId: Int,
+        userId: Int,
+        rank: Int,
+        time: Duration
+    ): Int {
+        val values = ContentValues().apply {
+            circuitId?.let {put(DataBase.LeaderboardEntry.COLUMN_CIRCUIT_ID, it)}
+            userId?. let {put(DataBase.LeaderboardEntry.COLUMN_USER_ID, it)}
+            rank?. let {put(DataBase.LeaderboardEntry.COLUMN_RANK, it)}
+            time?. let {put(DataBase.LeaderboardEntry.COLUMN_TIME, it.toString())}
+        }
+        return db.update(
+            DataBase.LeaderboardEntry.TABLE_NAME,
+            values,
+            "${DataBase.LeaderboardEntry.COLUMN_LEADERBOARD_ID} = ?",
+            arrayOf(leaderboardId.toString())
+        )
+    }
+
+    fun deleteLeaderboard(leaderboardId: Int): Int {
+        return db.delete(
+            DataBase.LeaderboardEntry.TABLE_NAME,
+            "${DataBase.LeaderboardEntry.COLUMN_LEADERBOARD_ID} = ?",
+            arrayOf(leaderboardId.toString())
+        )
+    }
+
 }
--- Cod/app/src/main/java/com/example/runpath/database/RunDAO.kt
+++ Cod/app/src/main/java/com/example/runpath/database/RunDAO.kt
@@ -1,4 +1,84 @@
 package com.example.runpath.database
 
-class RunDAO {
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.time.Instant
+import kotlin.time.Duration
+
+class RunDAO(context: Context, dbHelper: SQLiteOpenHelper) {
+
+    private val db: SQLiteDatabase = dbHelper.writableDatabase
+
+    fun insertRun(
+        runId: Int,
+        userId: Int,
+        circuitId: Int,
+        startTime: Instant,
+        endTime: Instant,
+        pauseTime: Duration,
+        timeTracker: Duration,
+        paceTracker: Double,
+        distanceTracker: Double
+        ): Long {
+        val values = ContentValues().apply {
+            put(DataBase.RunEntry.COLUMN_RUN_ID, runId)
+            put(DataBase.RunEntry.COLUMN_USER_ID, userId)
+            put(DataBase.RunEntry.COLUMN_CIRCUIT_ID, circuitId)
+            put(DataBase.RunEntry.COLUMN_START_TIME, startTime.toString())
+            put(DataBase.RunEntry.COLUMN_END_TIME, endTime.toString())
+            put(DataBase.RunEntry.COLUMN_PAUSE_TIME, pauseTime.toString())
+            put(DataBase.RunEntry.COLUMN_TIME_TRACKER, timeTracker.toString())
+            put(DataBase.RunEntry.COLUMN_PACE_TRACKER, paceTracker)
+            put(DataBase.RunEntry.COLUMN_DISTANCE_TRACKER, distanceTracker)
+        }
+        return db.insert(DataBase.RunEntry.TABLE_NAME, null, values)
+    }
+
+    fun getRunById(runId: Int): Cursor {
+        return db.rawQuery(
+            "SELECT * FROM ${DataBase.RunEntry.TABLE_NAME} WHERE ${DataBase.RunEntry.COLUMN_RUN_ID} = ?",
+            arrayOf(runId.toString())
+        )
+    }
+
+    fun updateRun(
+        runId: Int,
+        userId: Int,
+        circuitId: Int,
+        startTime: Instant,
+        endTime: Instant,
+        pauseTime: Duration,
+        timeTracker: Duration,
+        paceTracker: Double,
+        distanceTracker: Double
+    ): Int {
+        val values = ContentValues().apply {
+            userId?. let {put(DataBase.RunEntry.COLUMN_USER_ID, it)}
+            circuitId?. let {put(DataBase.RunEntry.COLUMN_CIRCUIT_ID, it)}
+            startTime?. let {put(DataBase.RunEntry.COLUMN_START_TIME, it.toString())}
+            endTime?. let {put(DataBase.RunEntry.COLUMN_PAUSE_TIME, it.toString())}
+            pauseTime?. let {put(DataBase.RunEntry.COLUMN_PAUSE_TIME, it.toString())}
+            timeTracker?. let {put(DataBase.RunEntry.COLUMN_TIME_TRACKER, it.toString())}
+            paceTracker?. let {put(DataBase.RunEntry.COLUMN_PACE_TRACKER, it)}
+            distanceTracker?. let {put(DataBase.RunEntry.COLUMN_DISTANCE_TRACKER, it)}
+        }
+        return db.update(
+            DataBase.RunEntry.TABLE_NAME,
+            values,
+            "${DataBase.RunEntry.COLUMN_RUN_ID} = ?",
+            arrayOf(runId.toString())
+        )
+    }
+
+    fun deleteRun(runId: Int): Int {
+        return db.delete(
+            DataBase.RunEntry.TABLE_NAME,
+            "${DataBase.RunEntry.COLUMN_RUN_ID} = ?",
+            arrayOf(runId.toString())
+        )
+    }
+    
 }
