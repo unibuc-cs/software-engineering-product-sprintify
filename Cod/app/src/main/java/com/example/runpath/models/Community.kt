5446dcb5e0e30c6678e34ab2c5ddefbaecb3a5be
bote112 <cosmin-adrian.botezatu@s.unibuc.ro>
2024-04-20 15:02:47 +0300

    Community + Community_Users

---
+++ Cod/app/src/main/java/com/example/runpath/models/Community.kt
@@ -0,0 +1,7 @@
+package com.example.runpath.models
+
+data class Community (
+    val communityID: Int,
+    val name: String,
+    val description: String
+)
---
+++ Cod/app/src/main/java/com/example/runpath/models/Community_Users.kt
@@ -0,0 +1,7 @@
+package com.example.runpath.models
+
+data class Community_Users (
+    val communityID: Int,
+    val userID: Int,
+    val dateJoined: String
+)
