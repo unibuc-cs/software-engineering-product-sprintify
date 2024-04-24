1beafc4062c9fc681455b5f70393b6aea0583caa
SerbanA01 <andrei.serban2@s.unibuc.ro>
2024-04-24 11:37:44 +0300

    homepage

--- Cod/app/src/main/java/com/example/runpath/ui/theme/HomePage.kt
+++ Cod/app/src/main/java/com/example/runpath/ui/theme/HomePage.kt
@@ -1,12 +1,13 @@
 package com.example.runpath.ui.theme
 
+import FeedReaderDbHelper
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 
 @Composable
-fun HomePage() {
+fun HomePage( dbHelper: FeedReaderDbHelper) {
     Column {
-        Text("Hello, world!")
+       
     }
 }
