ff4f0cb8fcda07370e5827752ab5e11a17d71afd
SerbanA01 <andrei.serban2@s.unibuc.ro>
2024-05-20 20:31:45 +0300

    Update UserDAO.kt

--- Cod/app/src/main/java/com/example/runpath/database/UserDAO.kt
+++ Cod/app/src/main/java/com/example/runpath/database/UserDAO.kt
@@ -7,45 +7,63 @@ import android.content.ContentValues
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.Cursor
 import androidx.compose.ui.platform.LocalContext
+import com.example.runpath.models.Post
+import com.example.runpath.models.User
+import com.google.firebase.firestore.FirebaseFirestore
 import java.time.LocalDateTime
 
 
+class UserDAO() {
 
-class UserDAO (context: Context, dbHelper: FeedReaderDbHelper) {
+    private val db = FirebaseFirestore.getInstance()
+    fun insertUser(user: User, onComplete: (User) -> Unit) {
+        val documentReference = db.collection("users").document()
+        val userId = documentReference.id
+        val newUser = user.copy(userId = userId)
 
-    private val db: SQLiteDatabase = dbHelper.writableDatabase
-    fun insertUser(username: String, email: String, password: String) : Long {
-        val values = ContentValues().apply {
-            put(DataBase.UserEntry.COLUMN_USERNAME, username)
-            put(DataBase.UserEntry.COLUMN_EMAIL, email)
-            put(DataBase.UserEntry.COLUMN_PASSWORD, password)
-            val dateCreated = LocalDateTime.now().toString()
-            put(DataBase.UserEntry.COLUMN_DATE_CREATED, dateCreated)
-        }
-        return db.insert(DataBase.UserEntry.TABLE_NAME, null, values)
+        documentReference.set(newUser)
+            .addOnSuccessListener {
+                onComplete(newUser)
+            }
+            .addOnFailureListener { e ->
+                println("Error adding document: $e")
+            }
+
     }
 
-    fun getUserById(userId: Int): Cursor {
-        return db.rawQuery(
-            "SELECT * FROM ${DataBase.UserEntry.TABLE_NAME} WHERE ${DataBase.UserEntry.COLUMN_USER_ID} = ?",
-            arrayOf(userId.toString())
-        )
+    fun getUserById(userId: Int){
+        db.collection("users")
+            .document(userId.toString())
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    println("${document.id} => ${document.data}")
+                } else {
+                    println("No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                println("Error getting document: $exception")
+            }
+
     }
 
     fun login(username: String, password: String): Int {
         val cursor = getUserByUsername(username)
         if (cursor.moveToFirst()) {
-            val storedPassword = cursor.getString(cursor.getColumnIndexOrThrow(DataBase.UserEntry.COLUMN_PASSWORD))
+            val storedPassword =
+                cursor.getString(cursor.getColumnIndexOrThrow(DataBase.UserEntry.COLUMN_PASSWORD))
             if (storedPassword == password) {
                 //pastram id-ul userului logat in SharedPreferences
-                val userId = cursor.getInt(cursor.getColumnIndexOrThrow(DataBase.UserEntry.COLUMN_USER_ID))
+                val userId =
+                    cursor.getInt(cursor.getColumnIndexOrThrow(DataBase.UserEntry.COLUMN_USER_ID))
                 cursor.close()
                 return userId
-            }        }
+            }
+        }
         cursor.close()
         return -1 // -1 indicates login failed
     }
-
 
 
     fun getUserByUsername(username: String): Cursor {
@@ -55,7 +73,13 @@ class UserDAO (context: Context, dbHelper: FeedReaderDbHelper) {
         )
     }
 
-    fun updateUser(userId: Int, username: String?, email: String?, password: String?, dataCreated: String?): Int {
+    fun updateUser(
+        userId: Int,
+        username: String?,
+        email: String?,
+        password: String?,
+        dataCreated: String?
+    ): Int {
         val values = ContentValues().apply {
             username?.let { put(DataBase.UserEntry.COLUMN_USERNAME, it) }
             email?.let { put(DataBase.UserEntry.COLUMN_EMAIL, it) }
