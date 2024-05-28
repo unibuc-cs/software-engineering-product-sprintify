730dd6c46be94fe6d92f970e76ae690dfe0ca405
SerbanA01 <andrei.serban2@s.unibuc.ro>
2024-05-28 13:42:46 +0300

    circuits

--- Cod/app/build.gradle.kts
+++ Cod/app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation ("com.google.maps.android:maps-ktx:5.0.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
     // Android Maps Compose composables for the Maps SDK for Android
     implementation ("com.google.maps.android:maps-compose:4.4.0")
     implementation(libs.androidx.material3.android)
--- Cod/app/src/main/java/com/example/runpath/ui/theme/CircuitsPage.kt
+++ Cod/app/src/main/java/com/example/runpath/ui/theme/CircuitsPage.kt
@@ -1,82 +1,43 @@
+import android.widget.ImageView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.runpath.R
 import com.example.runpath.database.CircuitDAO
 import com.example.runpath.database.SessionManager
 import com.example.runpath.models.Circuit
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.LatLng as GoogleLatLng
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.PolylineOptions
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.Polyline
-import com.google.maps.android.compose.rememberCameraPositionState
-import kotlinx.coroutines.launch
-
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.maps.model.LatLng
+import com.squareup.picasso.Picasso
 
 
 @Composable
-fun CircuitMap(circuits: List<Circuit>) {
-    val context = LocalContext.current
-    val cameraPositionState = rememberCameraPositionState()
+fun CircuitMap(circuit: Circuit): String {
+    val apiKey = "AIzaSyBcDs0jQqyNyk9d1gSpk0ruLgvbd9pwZrU"
+    val baseUrl = "https://maps.googleapis.com/maps/api/staticmap"
+    val size = "900x1500"
+    val zoom = "16"
+    val path = circuit.route?.joinToString("|") { "${it.lat},${it.lng}" }
+    val style = "feature:all|element:labels|visibility:off"
+    val color = "0xFFA500" // portocaliu
 
-    val mapProperties by remember {
-        mutableStateOf(
-            MapProperties(
-                mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context, R.raw.circuit_map)
-            )
-        )
-    }
-
-    // Store the latest circuits value using rememberUpdatedState
-    val currentCircuits by rememberUpdatedState(newValue = circuits)
-
-    LaunchedEffect(currentCircuits) {
-        if (currentCircuits.isNotEmpty() && currentCircuits.first().route.isNotEmpty()) {
-            val firstRoutePoint = currentCircuits.first().route!!.first()
-            cameraPositionState.move(CameraUpdateFactory.newLatLngZoom(GoogleLatLng(firstRoutePoint.lat, firstRoutePoint.lng), 10f))
-        }
-    }
-
-    GoogleMap(
-        modifier = Modifier.fillMaxSize().height(300.dp),
-        properties = mapProperties,
-        cameraPositionState = cameraPositionState
-    ) {
-        currentCircuits.forEach { circuit ->
-            circuit.route?.let { route ->
-                Polyline(
-                    points = route.map { GoogleLatLng(it.lat, it.lng) },
-                    color = Color.Red // Adjust color as needed
-                )
-            }
-        }
-    }
+    return "$baseUrl?size=$size&zoom=$zoom&path=color:$color|$path&style=$style&key=$apiKey"
 }
-
 @Composable
 fun CircuitsPage(navController: NavController, sessionManager: SessionManager) {
 
@@ -99,7 +60,7 @@ fun CircuitsPage(navController: NavController, sessionManager: SessionManager) {
     ) {
         Text(
             text = "Circuits",
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp).align(Alignment.TopCenter)
         )
 
         // Circuits feed
@@ -110,17 +71,25 @@ fun CircuitsPage(navController: NavController, sessionManager: SessionManager) {
             itemsIndexed(circuits) { index, circuit ->
                 Column {
                     Text(
-                        text = circuit.name,
-                        modifier = Modifier.padding(16.dp)
+                        text = circuit.name
                     )
 
                     val route = circuit.route
                     if (route != null) {
-                        // Call your CircuitMap Composable here
-                        CircuitMap(circuits = listOf(circuit))
+                        val imageUrl = CircuitMap(circuit = circuit)
+                        AndroidView(
+                            factory = { context ->
+                                ImageView(context).apply {
+                                    Picasso.get().load(imageUrl).into(this)
+                                }
+                            },
+                            modifier = Modifier.width(1500.dp).height(500.dp)
+                        )
                     }
                 }
             }
         }
     }
+
+
 }
