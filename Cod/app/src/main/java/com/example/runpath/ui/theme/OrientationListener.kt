69a0c1d0d89e699e7a09d3036d2d7c961215db96
pUmA73 <stefanpp77@yahoo.com>
2024-06-14 17:59:09 +0300

    added orientation class

---
+++ Cod/app/src/main/java/com/example/runpath/ui/theme/OrientationListener.kt
@@ -0,0 +1,45 @@
+package com.example.runpath.ui.theme
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+
+class OrientationListener(context: Context) : SensorEventListener {
+    private val sensorManager: SensorManager =
+        context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val accelerometer: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    private val magnetometer: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+    private val gravity = FloatArray(3)
+    private val geomagnetic = FloatArray(3)
+    private val orientation = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+
+    var azimuth: Float = 0f
+        private set
+
+    fun startListening() {
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    fun stopListening() {
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        when(event.sensor.type) {
+            Sensor.TYPE_ACCELEROMETER -> System.arraycopy(event.values, 0, gravity, 0, gravity.size)
+            Sensor.TYPE_MAGNETIC_FIELD -> System.arraycopy(event.values, 0, geomagnetic, 0, geomagnetic.size)
+        }
+
+        if(SensorManager.getRotationMatrix(rotationMatrix, null, gravity, geomagnetic)) {
+            SensorManager.getOrientation(rotationMatrix, orientation)
+            azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+}
